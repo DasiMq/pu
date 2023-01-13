8cfbec094c9ffd4c50d8f20688de832cfa0b1e68
Dasikaran Selvaraj <dasikarans.19@uom.lk>
2023-01-13 11:09:34 +0530

    getData called

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,18 +20,12 @@ export class AppComponent {
       console.log(data)
       this.channels = data.items
     })
+
+    this.getData();
   }
 
   getData() {
-    // const urlMetadata = require('url-metadata')
-    // urlMetadata('http://bit.ly/2ePIrDy').then(
-    //   function (metadata) { // success handler
-    //     console.log(metadata)
-    //   },
-    //   function (error) { // failure handler
-    //     console.log(error)
-    //   })
-    
+
     var channelName = this.channelName.nativeElement.value
 
     this.youtube.getChannels(channelName).subscribe((data) => {
